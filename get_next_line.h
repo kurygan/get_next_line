e6c88113b62bee0b544a5752b3b2ecf17de2ee36
Kurygan <tylerlover911@Mac-du-Club.local>
2024-12-14 19:36:27 +0100

    added mandatory files

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkettab <mkettab@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/14 19:01:21 by mkettab           #+#    #+#             */
+/*   Updated: 2024/12/14 19:04:13 by mkettab          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+typedef struct s_list
+{
+	char *content;
+	struct s_list *next;
+} t_list;
+
+#endif
